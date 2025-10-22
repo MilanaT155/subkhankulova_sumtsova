94215df732759a3865000d1502e1a1a3f7d3db08
subkhankulova.kr2955 <subkhankulova.kr2955@C0M31-L2>
2025-10-22 14:20:20 +0500

    Добавьте файлы проекта.

---
+++ subkhankulova_sumtsova/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double a = 15;
+        double b = 3;
+
+        double sum = a + b;
+        double multiply = a * b;
+        double divide = a / b;
+
+        Console.WriteLine($"Сумма: {sum}");
+        Console.WriteLine($"Умножение: {multiply}");
+        Console.WriteLine($"Деление: {divide}");
+    }
+}
